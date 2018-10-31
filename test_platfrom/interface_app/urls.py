460b81f2067b733b284be2a936d80e151ba5bc44
surf2018 <ecuster@126.com>
2018-11-01 07:10:18 +0800

    add user_app的model ,view,template单元测试

--- test_platfrom/interface_app/urls.py
+++ test_platfrom/interface_app/urls.py
@@ -14,15 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path
+from interface_app import views
+# from project_app import views
+
 urlpatterns = [
-    path('admin/',admin.site.urls),
-    path('user/',include('user_app.urls')),
-    # path('login/',views.index),
-    # path('login_action/',views.login_action),
-    # path('logout/',views.logout_action),
-    path('project/',include('project_app.urls')),
-    path('module/',include('project_app.urls')),
-    path('interface/',include('interface_app.urls')),
+    path('case_manager/',views.caselist),
 
 ]
--- test_platfrom/interface_app/views.py
+++ test_platfrom/interface_app/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
+
+
+# Create your views here.
+#
+@login_required
+def caselist(request):
+    username = request.session.get('username', '')
+    type = request.GET['type']
+    if (type == ''):
+        type = 'caselist'
+    context = {'username': username,'type': type}
+    return render(request, 'case/testcase.html', context)
+
+
+# Create your views here.
