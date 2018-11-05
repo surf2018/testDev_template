0be6e4691bcc1279f59706d6ceb2fd05a977cdf8
surf2018 <ecuster@126.com>
2018-11-05 08:19:00 +0800

    add debug tools

--- test_platfrom/interface_app/models.py
+++ test_platfrom/interface_app/models.py
@@ -1,3 +1,19 @@
-from django.db import models
-
 # Create your models here.
+import sys
+sys.path.insert(0, '..')
+from django.db import models
+from ...test_platfrom.project_app.models.project_models import Project
+from ..project_app.models.module_models import Module
+
+class Case(models.Model):
+    name = models.CharField(max_length=20)
+    url = models.CharField(max_length=100)
+    method = models.CharField(max_length=20)
+    type = models.CharField(max_length=20)
+    header = models.CharField(max_length=200)
+    data = models.CharField(max_length=200)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    model = models.ForeignKey(Module, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.name
+
