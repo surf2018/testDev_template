a0727893420daf338ca0d002dcba37ae81701119
surf2018 <ecuster@126.com>
2018-11-16 16:10:35 +0800

    接口实现edit testcase

--- test_platfrom/interface_app/views/testcase_views.py
+++ test_platfrom/interface_app/views/testcase_views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from django.shortcuts import render
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponseRedirect,HttpResponse
+from django.http import HttpResponseRedirect, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 import requests
 import json
@@ -16,12 +16,14 @@ from django.db.models import Q
 from ..case_forms import CaseForm
 # Create your views here.
 #
+
+
 @login_required
 def caselist(request):
     username = request.session.get('username', '')
     type = request.GET['type']
     if (type == '' or type == 'caselist'):
-        case=Case.objects.all()
+        case = Case.objects.all()
         # 分页
         paginator = Paginator(case, 5)
         page = request.GET.get('page', 1)
@@ -33,101 +35,83 @@ def caselist(request):
             case = paginator.page(1)
         except EmptyPage:
             case = paginator.page(paginator.num_pages)
-        context = {'username': username,'type': type, 'cases':case}
+        context = {'username': username, 'type': type, 'cases': case}
         return render(request, 'case/testcase.html', context)
     if(type == 'create'):
-        pros=Project.objects.all()
-        mods=Module.objects.all()
-        context= {'username': username,'type': type,'pros':pros,'mods':mods}
+        pros = Project.objects.all()
+        mods = Module.objects.all()
+        context = {
+            'username': username,
+            'type': type,
+            'pros': pros,
+            'mods': mods}
         return render(request, 'case/add_case.html', context)
 
-    elif(type=='debug'):
+    elif(type == 'debug'):
         context = {'username': username, 'type': type}
-        return render(request,'case/api_debug.html',context)
+        return render(request, 'case/api_debug.html', context)
+
+#点击debug的处理
 @csrf_exempt
 def debug_ajax(request):
-    if(request.method=='POST'):
-        url=request.POST['url']
-        method=request.POST['method']
-        type=request.POST['type']
-        header=json.loads(request.POST['header'])
-        data=request.POST['parameter']
+    if(request.method == 'POST'):
+        url = request.POST['url']
+        method = request.POST['method']
+        type = request.POST['type']
+        header = json.loads(request.POST['header'])
+        data = request.POST['parameter']
         print(data)
-        data=json.loads(data,strict=False)
+        #转换为json格式
+        data = json.loads(data, strict=False)
         print(data)
         # data=eval(data)
-        if(method=='post'):
-            #post 请求
-            if(type!='json'):
+        if(method == 'post'):
+            # post 请求
+            if(type != 'json'):
                 if ('file' in data.keys()):
                     response = requests.post(url, files=data)
                     print(response)
                 else:
-                    response=requests.post(url,data=data,headers=header)
-                    print("post"+response.text+"status code:"+str(response.status_code))
+                    response = requests.post(url, data=data, headers=header)
+                    print("post" +
+                          response.text +
+                          "status code:" +
+                          str(response.status_code))
             else:
+                #json数据
                 print("post 请求json")
-                data=json.dumps(data)
-                response=requests.post(url,data=data,headers=header)
-                print("post" + response.text + "status code:" + str(response.status_code))
-        elif(method=='get'):
-            #get 请求
+                data = json.dumps(data)
+                response = requests.post(url, data=data, headers=header)
+                print("post" +
+                      response.text +
+                      "status code:" +
+                      str(response.status_code))
+        elif(method == 'get'):
+            # get 请求
             print("get 请求")
-            if(type!='json'):
-                response=requests.get(url,params=data,headers=header)
-                print("get:"+response.text+"status code:"+str(response.status_code))
+            if(type != 'json'):
+                response = requests.get(url, params=data, headers=header)
+                print("get:" +
+                      response.text +
+                      "status code:" +
+                      str(response.status_code))
             else:
                 data = json.dumps(data)
-                response=requests.get(url,params=data,headers=header)
-                print("get:"+response.text+"statu code:"+str(response.status_code))
+                response = requests.get(url, params=data, headers=header)
+                print("get:" +
+                      response.text +
+                      "statu code:" +
+                      str(response.status_code))
         return HttpResponse(response)
     else:
         username = request.session.get('username', '')
         context = {'username': username, 'type': 'debug'}
-        return render(requests,'case/api_debug.html',context)
+        return render(requests, 'case/api_debug.html', context)
 
+#保存新建数据
 def saveDate(request):
-    username=request.POST['username']
-    userid=User.objects.get(username=username).id
-    proid=request.POST['proid']
-    modid=request.POST['modid']
-    name=request.POST['name']
-    url=request.POST['url']
-    method=request.POST['method']
-    type=request.POST['type']
-    header=request.POST['header']
-    parameter=request.POST['parameter']
-    status=request.POST['status']
-    print(status)
-    case=Case(name=name,url=url,method=method,type=type,header=header,data=parameter,status=status,create_user_id=userid,model_id=modid,project_id=proid)
-    case.save()
-    return HttpResponse('save ok')
-def selectAjax(request):
-    proId=request.GET['para']
-    modeList={}
-    #select modules
-    mods=Module.objects.filter(project__id=proId)
-    for mod in mods:
-        modeList[mod.id]=mod.name
-    results=json.dumps(modeList)
-    print(results)
-    return HttpResponse(results,content_type='application/json')
-def debugCase(request,caseid):
-    pros=Project.objects.all()
-    mods=Module.objects.all()
-    cases=Case.objects.get(id=caseid)
-    proname=cases.project.name
-    print(proname)
-    modname=cases.model.name
-    print(modname)
-    username = request.session.get('username', '')
-    context = {'username': username, 'case':cases,'proname':proname,'modname':modname,'pros':pros,'mods':mods,'type':'debug'}
-    print(context)
-    return render(request, 'case/api_debug.html', context)
-def updateDate(request):
     username = request.POST['username']
     userid = User.objects.get(username=username).id
-    caseid=request.POST['caseid']
     proid = request.POST['proid']
     modid = request.POST['modid']
     name = request.POST['name']
@@ -138,22 +122,109 @@ def updateDate(request):
     parameter = request.POST['parameter']
     status = request.POST['status']
     print(status)
-    Case.objects.filter(id=caseid).update(name=name,url=url,method=method,type=type,header=header,data=parameter,status=status,create_user_id=userid,model_id=modid,project_id=proid)
+    case = Case(
+        name=name,
+        url=url,
+        method=method,
+        type=type,
+        header=header,
+        data=parameter,
+        status=status,
+        create_user_id=userid,
+        model_id=modid,
+        project_id=proid)
+    case.save()
+    return HttpResponse('save ok')
+
+#select联动接口
+def selectAjax(request):
+    proId = request.GET['para']
+    modeList = {}
+    # select modules
+    mods = Module.objects.filter(project__id=proId)
+    for mod in mods:
+        modeList[mod.id] = mod.name
+    results = json.dumps(modeList)
+    print(results)
+    return HttpResponse(results, content_type='application/json')
+
+
+def debugCase(request, caseid):
+    pros = Project.objects.all()
+    mods = Module.objects.all()
+    cases = Case.objects.get(id=caseid)
+    proname = cases.project.name
+    print(proname)
+    modname = cases.model.name
+    print(modname)
+    username = request.session.get('username', '')
+    context = {
+        'username': username,
+        'case': cases,
+        'proname': proname,
+        'modname': modname,
+        'pros': pros,
+        'mods': mods,
+        'type': 'debug'}
+    result=json.dumps(context)
+    return HttpResponse(result, content_type='application/json')
+    # return render(request, 'case/api_debug.html', context)
+
+#editcase and update case
+def updateDate(request):
+    username = request.POST['username']
+    userid = User.objects.get(username=username).id
+    caseid = request.POST['caseid']
+    proid = request.POST['proid']
+    modid = request.POST['modid']
+    name = request.POST['name']
+    url = request.POST['url']
+    method = request.POST['method']
+    type = request.POST['type']
+    header = request.POST['header']
+    parameter = request.POST['parameter']
+    status = request.POST['status']
+    print(status)
+    Case.objects.filter(
+        id=caseid).update(
+        name=name,
+        url=url,
+        method=method,
+        type=type,
+        header=header,
+        data=parameter,
+        status=status,
+        create_user_id=userid,
+        model_id=modid,
+        project_id=proid)
     return HttpResponse('Update ok')
-def delCase(request,caseid):
+
+
+def delCase(request, caseid):
     # 删除数据库
     Case.objects.filter(id=caseid).delete()
     return HttpResponseRedirect("/interface/case_manager/?type=caselist")
 
+#search case (name,url,method,project name,method name )
 def searchCase(request):
     query_text = request.GET['search']
     if (query_text == ""):
         caseInfo = Case.objects.all()
         # contex={"error":"请输入搜索字符"}
-        return HttpResponseRedirect("/interface/case_manager/?type=caselist", caseInfo)
+        return HttpResponseRedirect(
+            "/interface/case_manager/?type=caselist", caseInfo)
     else:
         username = request.session.get('username', '')
-        caseInfo = Case.objects.filter(Q(name__icontains=query_text)|Q(url__icontains=query_text)|Q(method__icontains=query_text)|Q(status__icontains=query_text)|Q(project__name__icontains=query_text)|Q(model__name__icontains=query_text))
+        #search case通过name,url,method,status,projectName,modelName
+        caseInfo = Case.objects.filter(
+            Q(
+                name__icontains=query_text) | Q(
+                url__icontains=query_text) | Q(
+                method__icontains=query_text) | Q(
+                    status__icontains=query_text) | Q(
+                        project__name__icontains=query_text) | Q(
+                            model__name__icontains=query_text))
+        #分页，每5个分页
         paginator = Paginator(caseInfo, 5)
         page = request.GET.get("page", 1)
         curpage = int(page)
@@ -163,6 +234,10 @@ def searchCase(request):
             caseInfo = paginator.page(1)
         except EmptyPage:
             caseInfo = paginator.page(paginator.num_pages)
-        context = {'cases': caseInfo, 'username': username, 'type': 'caselist', 'search':query_text}
+        context = {
+            'cases': caseInfo,
+            'username': username,
+            'type': 'caselist',
+            'search': query_text}
         return render(request, "case/testcase.html", context)
 # Create your views here.
--- test_platfrom/static/bootstrap/js/caseManager.js
+++ test_platfrom/static/bootstrap/js/caseManager.js
@@ -6,42 +6,42 @@ $('#send').click(function () {
         let req_type = $('input[name="req_type"]:checked').val();
         let req_header = $('#req_header').val();
         let req_parameter = $('#req_parameter').val();
-        if(req_name==''){
+        if (req_name == '') {
             window.alert("name不能为空");
-             $('#request-process-patent').html("")
+            $('#request-process-patent').html("")
             return false
         }
         if (req_url == "") {
             window.alert("URL不能为空");
-             $('#request-process-patent').html("")
+            $('#request-process-patent').html("")
             return false
         }
         //check url
-        else{
+        else {
             //判断URL地址的正则表达式为:http(s)?://([\w-]+\.)+[\w-]+(/[\w- ./?%&=]*)?
             //下面的代码中应用了转义字符"\"输出一个字符"/"
-            var Expression=/http(s)?:\/\/([\w-]+\.)+[\w-]+(\/[\w- .\/?%&=]*)?/;
-            var objExp=new RegExp(Expression);
+            var Expression = /http(s)?:\/\/([\w-]+\.)+[\w-]+(\/[\w- .\/?%&=]*)?/;
+            var objExp = new RegExp(Expression);
 
-            if(objExp.test(req_url) != true){
+            if (objExp.test(req_url) != true) {
                 alert("网址格式不正确！请重新输入");
                 $('#request-process-patent').html("")
                 return false;
             }
         }
-        if(req_header==""){
-            req_header="{}";
+        if (req_header == "") {
+            req_header = "{}";
         }
         else {
             //单引号转换为双引号
-            req_header = req_header.replace(/\'/g,"\"");
+            req_header = req_header.replace(/\'/g, "\"");
         }
         if (req_parameter == "") {
             req_parameter = "{}";
         }
         else {
             //单引号转换为双引号
-            req_parameter = req_parameter.replace(/\'/g,"\"");
+            req_parameter = req_parameter.replace(/\'/g, "\"");
             // alert(req_parameter)
         }
         var datas = {
@@ -54,99 +54,99 @@ $('#send').click(function () {
         }
         console.log(datas);
         $.ajax({
-            type:"POST",
-            url:"/interface/debug_ajax/",
-            data:datas,
-            success: function(ret){
-                    console.log("debug_ajax success")
-                    console.log(ret)
-                    $('#result').val(ret)
-                    $('#request-process-patent').html("Status:200 OK")
-                    },
-            error:function (ret) {
+            type: "POST",
+            url: "/interface/debug_ajax/",
+            data: datas,
+            success: function (ret) {
+                console.log("debug_ajax success")
+                console.log(ret)
+                $('#result').val(ret)
+                $('#request-process-patent').html("Status:200 OK")
+            },
+            error: function (ret) {
                 console.log("debug_ajax fail")
                 $('#request-process-patent').html("失败")
 
             }
         })
     }
-    );
+);
 
-function createDebug(){
-  window.location.href="/interface/case_manager/?type=create"
+function createDebug() {
+    window.location.href = "/interface/case_manager/?type=create"
 }
 
 
-$('#return').click(function (){
-    window.location.href="/interface/case_manager/?type=caselist"
-}
+$('#return').click(function () {
+        window.location.href = "/interface/case_manager/?type=caselist"
+    }
 )
 //update data
 $('#update').click(function () {
         $('#request-process-patent').html("正在更新数据...");
-        let req_username=$("#navbar_user").text();
-        let req_caseid=window.location.href.split('/')[5]
-        let req_proid=$("#pro-dropdown").val();
-        let req_modid=$('#mod-dropdown').val();
+        let req_username = $("#navbar_user").text();
+        let req_caseid = window.location.href.split('/')[5]
+        let req_proid = $("#pro-dropdown").val();
+        let req_modid = $('#mod-dropdown').val();
         let req_name = $('#req_name').val();
         let req_url = $('#req_url').val();
-        let req_status=$('input[name="req_status"]:checked').val();
+        let req_status = $('input[name="req_status"]:checked').val();
         // alert(req_status)
         let req_method = $('input[name="req_method"]:checked').val();
         let req_type = $('input[name="req_type"]:checked').val();
         let req_header = $('#req_header').val();
         let req_parameter = $('#req_parameter').val();
-        if(req_proid=="" || req_modid==""){
+        if (req_proid == "" || req_modid == "") {
             window.alert("请选择模块和项目");
             return false
         }
-        if(req_name==''){
+        if (req_name == '') {
             window.alert("name不能为空");
-             $('#request-process-patent').html("");
+            $('#request-process-patent').html("");
             return false
         }
         if (req_url == "") {
             window.alert("URL不能为空");
-             $('#request-process-patent').html("");
+            $('#request-process-patent').html("");
             return false
         }
         //check url
-        else{
+        else {
             //判断URL地址的正则表达式为:http(s)?://([\w-]+\.)+[\w-]+(/[\w- ./?%&=]*)?
             //下面的代码中应用了转义字符"\"输出一个字符"/"
-            var Expression=/http(s)?:\/\/([\w-]+\.)+[\w-]+(\/[\w- .\/?%&=]*)?/;
-            var objExp=new RegExp(Expression);
+            var Expression = /http(s)?:\/\/([\w-]+\.)+[\w-]+(\/[\w- .\/?%&=]*)?/;
+            var objExp = new RegExp(Expression);
 
-            if(objExp.test(req_url) != true){
+            if (objExp.test(req_url) != true) {
                 alert("网址格式不正确！请重新输入");
                 $('#request-process-patent').html("");
                 return false;
             }
         }
         //check header
-        if(req_header==""){
-            req_header="{}";
+        if (req_header == "") {
+            req_header = "{}";
         }
-        else{
+        else {
             //单引号转换成双引号
-            req_header = req_header.replace(/\'/g,"\"");
+            req_header = req_header.replace(/\'/g, "\"");
         }
         if (req_parameter == "") {
             req_parameter = "{}";
         }
         else {
             //单引号转换为双引号
-            req_parameter = req_parameter.replace(/\'/g,"\"");
+            req_parameter = req_parameter.replace(/\'/g, "\"");
             // alert(req_parameter)
         }
         var datas = {
-            "username":req_username,
-            "caseid":req_caseid,
-            "proid":req_proid,
-            "modid":req_modid,
+            "username": req_username,
+            "caseid": req_caseid,
+            "proid": req_proid,
+            "modid": req_modid,
             "name": req_name,
             "url": req_url,
-            "status":req_status,
+            "status": req_status,
             "method": req_method,
             "type": req_type,
             "header": req_header,
@@ -154,137 +154,166 @@ $('#update').click(function () {
         }
         console.log(datas);
         $.ajax({
-            type:"POST",
-            url:"/interface/update/",
-            data:datas,
-            success: function(ret){
-                    console.log("debug_ajax success")
-                    console.log(ret)
-                    $('#request-process-patent').html("Save ok")
-                    },
-            error:function (ret) {
+            type: "POST",
+            url: "/interface/update/",
+            data: datas,
+            success: function (ret) {
+                console.log("debug_ajax success")
+                console.log(ret)
+                $('#request-process-patent').html("Save ok")
+            },
+            error: function (ret) {
                 console.log("debug_ajax fail")
                 $('#request-process-patent').html("Save fail")
 
             }
         })
     }
-    );
+);
 //save data
 $('#save').click(function () {
         $('#request-process-patent').html("正在保存数据...");
-        let req_username=$("#navbar_user").text();
-        let req_proid=$("#pro-dropdown").val();
-        let req_modid=$('#mod-dropdown').val();
+        let req_username = $("#navbar_user").text();
+        let req_proid = $("#pro-dropdown").val();
+        let req_modid = $('#mod-dropdown').val();
         let req_name = $('#req_name').val();
         let req_url = $('#req_url').val();
-        let req_status=$('input[name="req_status"]:checked').val();
+        let req_status = $('input[name="req_status"]:checked').val();
         let req_method = $('input[name="req_method"]:checked').val();
         let req_type = $('input[name="req_type"]:checked').val();
         let req_header = $('#req_header').val();
         let req_parameter = $('#req_parameter').val();
-        if(req_proid=="" || req_modid==""){
+        if (req_proid == "" || req_modid == "") {
             window.alert("请选择模块和项目");
             return false
         }
-        if(req_name==''){
+        if (req_name == '') {
             window.alert("name不能为空");
-             $('#request-process-patent').html("");
+            $('#request-process-patent').html("");
             return false
         }
         if (req_url == "") {
             window.alert("URL不能为空");
-             $('#request-process-patent').html("");
+            $('#request-process-patent').html("");
             return false
         }
         //check url
-        else{
+        else {
             //判断URL地址的正则表达式为:http(s)?://([\w-]+\.)+[\w-]+(/[\w- ./?%&=]*)?
             //下面的代码中应用了转义字符"\"输出一个字符"/"
-            var Expression=/http(s)?:\/\/([\w-]+\.)+[\w-]+(\/[\w- .\/?%&=]*)?/;
-            var objExp=new RegExp(Expression);
+            var Expression = /http(s)?:\/\/([\w-]+\.)+[\w-]+(\/[\w- .\/?%&=]*)?/;
+            var objExp = new RegExp(Expression);
 
-            if(objExp.test(req_url) != true){
+            if (objExp.test(req_url) != true) {
                 alert("网址格式不正确！请重新输入");
                 $('#request-process-patent').html("");
                 return false;
             }
         }
         //check header
-        if(req_header==""){
-            req_header="{}";
+        if (req_header == "") {
+            req_header = "{}";
         }
-        else{
+        else {
             //单引号转换成双引号
-            req_header = req_header.replace(/\'/g,"\"");
+            req_header = req_header.replace(/\'/g, "\"");
         }
         if (req_parameter == "") {
             req_parameter = "{}";
         }
         else {
             //单引号转换为双引号
-            req_parameter = req_parameter.replace(/\'/g,"\"");
+            req_parameter = req_parameter.replace(/\'/g, "\"");
             // alert(req_parameter)
         }
         var datas = {
-            "username":req_username,
-            "proid":req_proid,
-            "modid":req_modid,
+            "username": req_username,
+            "proid": req_proid,
+            "modid": req_modid,
             "name": req_name,
             "url": req_url,
-            "status":req_status,
+            "status": req_status,
             "method": req_method,
             "type": req_type,
             "header": req_header,
             "parameter": req_parameter
-        }
+        };
         console.log(datas);
         $.ajax({
-            type:"POST",
-            url:"/interface/save/",
-            data:datas,
-            success: function(ret){
-                    console.log("debug_ajax success")
-                    console.log(ret)
-                    $('#request-process-patent').html("Save ok")
-                    },
-            error:function (ret) {
+            type: "POST",
+            url: "/interface/save/",
+            data: datas,
+            success: function (ret) {
+                console.log("debug_ajax success")
+                console.log(ret)
+                $('#request-process-patent').html("Save ok")
+            },
+            error: function (ret) {
                 console.log("debug_ajax fail")
                 $('#request-process-patent').html("Save fail")
 
             }
         })
     }
-    );
+);
 //select的联动
 $('#pro-dropdown').change(function (e) {
-    var values=$(this).val();
+    var values = $(this).val();
     $.ajax({
         url: '/interface/seletAjax/?para=' + values,
         type: 'GET',
         success: function (results) {
             console.log(results)
             if (results) {
-                var optionstring="";
-                $.each(results, function (key,value) {
-                    optionstring += "<option value=\""+key+"\">"+value+"</option>"
+                var optionstring = "";
+                $.each(results, function (key, value) {
+                    optionstring += "<option value=\"" + key + "\">" + value + "</option>"
                 });
-               $("#mod-dropdown").html("<option value=''>请选择模块</option> "+optionstring)
+                $("#mod-dropdown").html("<option value=''>请选择模块</option> " + optionstring)
             }
         },
         error: function () {
 
         }
     })
-})
+});
+
 //delete case
-function delcase(caseid,casename) {
+function delcase(caseid, casename) {
     // alert("是否需要删除项目："+pname)
     var r = confirm("确定删除" + casename + " case？");
-    if ( r  == true) {
-      window.location.href = "/interface/delCase/"+caseid+"/";
+    if (r == true) {
+        window.location.href = "/interface/delCase/" + caseid + "/";
     }
     else {
-      window.location.href = "/interface/case_manager/?type=caselist";
+        window.location.href = "/interface/case_manager/?type=caselist";
     }
+}
+
+//点击debug,js传送caseid对应的信息给后台,成功后返回case的老数据
+function debugCaseAjax(caseid) {
+    //跳转到api_debug.html
+    window.location.href=''
+    var datas = {
+        "caseid": caseid,
+    };
+    $.ajax({
+        url: "/interface/debugCase/"+caseid+"/",
+        type: 'POST',
+        success: function (results) {
+            console.log(results)
+            if (results) {
+                //成功跳转到api_debug.html
+                var optionstring = "";
+                $.each(results, function (key, value) {
+                    optionstring += "<option value=\"" + key + "\">" + value + "</option>"
+                });
+                $("#mod-dropdown").html("<option value=''>请选择模块</option> " + optionstring)
+            }
+        },
+        error: function () {
+
+        }
+    })
+
 }
